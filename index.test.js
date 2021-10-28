98dafbdee7d7a96e06d22839885ebbee42aa4900
Sophie Carter <secarter@hotmail.com>
2021-10-28 14:19:57 +0100

    jest walkthrough code upload

---
+++ index.test.js
@@ -0,0 +1,19 @@
+const index  = require('./index')
+
+let dougie = index.dougie
+
+
+
+test('that our first test passes', () => {
+
+    expect(true).toBe(true)
+
+})
+
+
+
+test('that dougie is orange', () => {
+
+    expect(dougie.colour).toBe('orange')
+
+})
